3378d8074431c82b63f67aae15dded39472ca254
Aissatou oury Diallo <aissatouourydiallo05@gmail.com>
2025-11-10 20:54:21 +0100

    modification enregistree

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -6,7 +6,20 @@ if [ -z "$1" ]; then
 fi
 
 VILLE=$1
-FICHIER_BRUT="meteo_brut.txt"
+FICHIER="meteo_brut.txt"
 
-curl -s "https://wttr.in/$VILLE" | sed 's/\x1B\[[0-9;]*m//g' > "$FICHIER_BRUT"
-echo "Donne meteo enregistree dans $FICHIER_BRUT"
+curl -s "https://wttr.in/$VILLE" | sed 's/\x1B\[[0-9;]*m//g' > "$FICHIER"
+
+if [ ! -s "$FICHIER" ]; then
+    echo "Erreur : impossible de récupérer les données météo."
+    exit 1
+fi
+
+echo "Donnees meteo enregistrees dans $FICHIER"
+
+TEMP_ACTUELLE=$(grep -o '[0-9]\+ *°C' "$FICHIER" | head -n1)
+TEMP_DEMAIN=$(grep "°C" "$FICHIER" | head -n 2 | tail -n 1 | grep -o '[0-9+-]\+ *°C')
+
+echo "Température actuelle : $TEMP_ACTUELLE"
+echo "Prévision demain : $TEMP_DEMAIN"
+
